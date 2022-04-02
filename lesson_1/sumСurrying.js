d2469cc814364b3dcef5eefdd8e42e5a30773f95
perovserg@mail.ru <perovserg@mail.ru>
2022-04-02 19:59:38 +0300

    feature: adds homework for lesson 1

--- lesson_1/sumСurrying.js
+++ lesson_1/sumСurrying.js
@@ -1,21 +1,20 @@
-const sum = (function() {
-    let acc = 0;
-    const innerFunc = (number) => {
-        acc += number || 0;
-        if (number === undefined) {
-            const result = acc;
-            acc = 0; // todo: есть ли способ избежать этого костыля для обнуления счетчика?
-            return result;
-        }
-        return innerFunc;
-    };
-    return innerFunc;
-})();
+// const sum = (function() {
+//     let acc = 0;
+//     const innerFunc = (number) => {
+//         acc += number || 0;
+//         if (number === undefined) {
+//             const result = acc;
+//             acc = 0; // todo: есть ли способ избежать этого костыля для обнуления счетчика?
+//             return result;
+//         }
+//         return innerFunc;
+//     };
+//     return innerFunc;
+// })();
+
+const sum = (num) => (num2) => num2 === undefined ? num : sum(num + num2);
 
 console.log(sum(1)(2)(3)(4)(5)()); // 15
 console.log(sum(10)(20)()) // 30
 console.log(sum(1)(2)(3)(4)(5)(0)(-10)()) // 5
-
-sum(1)(2)(3)
-sum(4)(5)(6)
-console.log(sum(7)(8)(9)(0)()) //45
+console.log(sum(7)(8)(9)(0)()) //24
