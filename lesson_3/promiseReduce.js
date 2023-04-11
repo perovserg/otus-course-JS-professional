fdcb18d0771dac100be93f8c4e1979c307ca481b
Sergey <perovserg@mail.ru>
2023-04-11 11:10:51 +0300

    added description

--- lesson_3/promiseReduce.js
+++ lesson_3/promiseReduce.js
@@ -1,3 +1,13 @@
+/*
+Написать функцию
+promiseReduce(asyncFunctions, reduce, initialValue)
+asyncFunctions - массив асинхронных функций, возвращающих промис
+reduce(memo, value) - функция, которая будет вызвана для каждого успешно завершившегося промиса.
+initialValue - стартовое значение для функции reduce
+promiseReduce последовательно вызывает переданные асинхронные функции
+и выполняет reduce функцию сразу при получении результата до вызова следующей асинхронной функции. Функция promiseReduce должна возвращать промис с конечным результатом.
+*/
+
 const fn1 = (value) => () => {
     console.log('fn1 >> value => ', value)
     return Promise.resolve(value)
